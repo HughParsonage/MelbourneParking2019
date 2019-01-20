9923b8d253df9e47d27367373ba1f12e1e9c6779
HughParsonage <hugh.parsonage@gmail.com>
2019-01-21 01:52:45 +1100

    Add 2019/1/20 (some missing)

--- data-raw/repeater.R
+++ data-raw/repeater.R
@@ -1,31 +1,40 @@
-Parking20190109 <- 
-  lapply(dir(path = "~/MelbourneParking2019/data-raw/2019/1/9",
-             pattern = "\\.tsv$",
-             full.names = TRUE),
-         fread) %>%
-  rbindlist
-
-fwrite()
-
-fwrite(Parking20190109[order(bay_id, Now), 
-                       .(bay_id,
-                         Occupied = as.integer(status == "Present"),
-                         Time = Now)],
-       tempf <- tempfile(fileext = ".tsv"),
-       sep = "\t")
-
-do_fread <- function(file.ctsv) {
-  o <- fread(file.ctsv, select = c("bay_id", "st_marker_id", "lat", "lon"))
-  o[, File := sub("\\.(c|t)sv$", "", basename(file.ctsv))]
-  o
+library(data.table)
+library(hutils)
+HourMin <- function(Time) {
+  paste0(formatC(hour(Time), width = 2, flag = "0"),
+         formatC(minute(Time), width = 2, flag = "0"),
+         formatC(second(Time), width = 2, flag = "0"))
 }
 
-Parking20190110_mtime <-
-  lapply(dir(path = "~/MelbourneParking2019/data-raw/2019/1/10",
-             pattern = "\\.(t|c)sv$",
-             full.names = TRUE),
-         do_fread) %>%
-  rbindlist(use.names = TRUE,
-            fill = TRUE)
+backoff <- -1
+lock_key <- as.character(sample(1e7:1e8, size = 1L))
+writeChar(lock_key, "fetch_lock", n = 8L)  # See if another program has spawned
 
-
+repeat ({
+  if (file.exists("fetch_killer")) {
+    break
+  }
+  Now <- Sys.time()
+  do_break <- FALSE
+  while (second(Now) %% 15L) {
+    if (file.exists("fetch_killer") || 
+        readChar("fetch_lock", n = 8L) != lock_key) {
+      do_break <- TRUE # need to break out of both
+      break
+    }
+    Sys.sleep(2 ^ backoff)
+    Now <- Sys.time()
+  }
+  if (do_break) {
+    break
+  }
+  # source a file so we can modify it as required
+  source("data-raw/fetch.R")
+  
+  if (res) {
+    cat(paste0(the_dir, "/", HourMin(Now), ".csv"), "failed.",
+        file = "data-raw/fetch_log.txt",
+        append = TRUE)
+    backoff <<- backoff + 1
+  }
+})
